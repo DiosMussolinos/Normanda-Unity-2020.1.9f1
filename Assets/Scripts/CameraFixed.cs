efdb1f9a5642da076341c7bedd7cea9ef5780582
DiosMussolinos <63793732+VergarusMussolinus@users.noreply.github.com>
2020-11-17 12:15:59 +0000

    nova organização

---
+++ Assets/Scripts/CameraFixed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFixed : MonoBehaviour
+{
+    private Transform player;
+
+    public float offsetX = 0f;
+    public float offsetY = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + offsetX, player.position.y + offsetY, transform.position.z);
+    }
+}
+
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    //private GameObject spriteRender;
+    private Animator animator;
+
+    public float WalkSpeed = 6f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Calling stuff
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //__movimento X & Y__\\
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(horizontal * WalkSpeed, vertical * WalkSpeed);
+
+
+        //__Animations__\\
+        //__MOVEMENT X__\\
+        if (horizontal != 0)
+        {
+            animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        }
+        else
+        {
+            animator.SetFloat("Speed", -1);
+        }
+
+        
+        //__MOVEMENT Y__\\
+        /*if (vertical != 0)
+        {
+            animator.SetFloat("Speed", Mathf.Abs(rb.velocity.y));
+        }
+        else
+        {
+            animator.SetFloat("Speed", -1);
+        }
+        */
+    }
+}
