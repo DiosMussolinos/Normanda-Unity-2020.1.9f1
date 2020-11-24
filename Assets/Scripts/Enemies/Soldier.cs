0f5aa29a706a5a4594ee573cf85060ab835abab6
DiosMussolinos <63793732+VergarusMussolinus@users.noreply.github.com>
2020-11-24 11:30:19 +0000

    Archer Finished

--- Assets/Scripts/Enemies/Soldier.cs
+++ Assets/Scripts/Enemies/Soldier.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class Soldier : MonoBehaviour
 {
+    //Behavior
+    public float speed;
+    public float stoppingDistance;
+    public float retreaDistance;
+    public float visionDistance;
+
+    //Control of shots
+    public float timeBtwShots;
+    public float startTimeBtwShots;
+
+    //Details of Enemy
+    public int archerLife;
+    public int CollisionDamage;
+    public int Damage;
+    public int gold;
+    public int experience;
+    public int level;
+    
+    //Calling stuff
+    public GameObject projectile;
+    public Transform player;
+
+    // Awake is called before Start
+    void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
