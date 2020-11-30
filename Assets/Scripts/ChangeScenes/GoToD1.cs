f83d805d7811564e5ba7a831a36d02e258b480ab
DiosMussolinos <63793732+VergarusMussolinus@users.noreply.github.com>
2020-11-30 22:19:55 +0000

    SIMPLE UI

--- Assets/Scripts/ChangeScenes/GoToD1.cs
+++ Assets/Scripts/ChangeScenes/GoToD1.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class GoToD1 : MonoBehaviour
 {
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+
             Debug.Log("Enter NIGGA");
             SceneManager.LoadScene("D1");
         }
