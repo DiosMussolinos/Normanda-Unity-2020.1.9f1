874ca30f435a55bcec37059566a7dea7fc5f2344
Rackday <nuno97teixeira@gmail.com>
2020-12-21 08:46:24 +0000

    Sound

---
+++ Assets/Scripts/Sound/PlayerSounds.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        audioSource.loop = true;
+    }
+
+    private void Update()
+    {
+        if (rb.velocity.x != 0 || rb.velocity.y != 0)
+        {
+            audioSource.UnPause();
+        }
+
+        else
+        {
+            audioSource.Pause();
+        }
+    }
+
+}
