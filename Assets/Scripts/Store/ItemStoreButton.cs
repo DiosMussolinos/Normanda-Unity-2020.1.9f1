e845bb66ff32070d54878cb2cc7fcf7e1cbf91e7
DiosMussolinos <63793732+VergarusMussolinus@users.noreply.github.com>
2020-12-23 15:31:57 +0000

    Some stupid shit to fix

--- Assets/Scripts/Store/ItemStoreButton.cs
+++ Assets/Scripts/Store/ItemStoreButton.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ItemStoreButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
 {
     public int buttonID;
     public Item itemData;
     [SerializeField] private Item thisItem;
+    
+    //Get Image and Description
+    public Image itemImage;
+    public Text itemName;
+    public Text itemDescription;
 
+    void Start()
+    {
+        itemImage.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+    }
 
     private Item GetThisItem()
     {
@@ -25,7 +35,20 @@ public class ItemStoreButton : MonoBehaviour, IPointerEnterHandler, IPointerClic
     public void OnPointerEnter(PointerEventData eventData)
     {
         GetThisItem();
-        
+        if (thisItem != null)
+        {
+            //Ter imagem a mostra
+            itemImage.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+            //Mudar de acordo com o item selecionado
+            itemImage.sprite = thisItem.itemSprite;
+
+            //Mudar Texto
+            //Name
+            itemName.GetComponent<Text>().text = thisItem.name;
+            //Description
+            itemDescription.GetComponent<Text>().text = thisItem.itemDes;
+        }
+
     }
 
     public void OnPointerClick(PointerEventData eventData)
