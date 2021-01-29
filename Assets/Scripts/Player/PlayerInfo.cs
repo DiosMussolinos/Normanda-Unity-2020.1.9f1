95679d4c5a7508c36ca77fb9644e1dcbb2e02220
DiosMussolinos <63793732+VergarusMussolinus@users.noreply.github.com>
2021-01-29 09:59:30 +0000

    123

---
+++ Assets/Scripts/Player/PlayerInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class PlayerInfo : MonoBehaviour
+{
+    public string username;
+    public int user_id;
+
+    public PlayerInfo(string uName, int uID)
+    {
+        username = uName;
+        user_id = uID;
+    }
+
+}
